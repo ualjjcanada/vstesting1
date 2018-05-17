5a16c561ffeda56283ddd5f46195d1a76ff86e82
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2018-05-17 13:53:02 +0200

    unit test project

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorNS
+{
+    public class Calculator
+    {
+       
+       
+
+        public double Sum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Minus(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double max(double num1, double num2)
+        {
+            if (num1 > num2) return num1;
+            
+            return num2;
+        }
+
+        public double min(double num1, double num2)
+        {
+            if (num1 < num2) return num1;
+
+            return num2;
+        }
+
+        public static String triangleType(int a, int b, int c)
+        {
+            String type;
+
+            if(a <= 0 || b <= 0 || c <= 0)
+            {
+             type = "Triangle not valid";
+            }
+            else if (a == b && b == c)
+            {
+                type = "Equilateral";
+            }
+            else if (b == c || a == b || c == a)
+            {
+                type = "Isosceles";
+            }
+            else {
+                type = "Scalene";
+            }
+            return type;
+        }
+
+        public int methodWithoutFullCoverage(int i)
+        {
+            if (i > 0) return 1;
+            else return -1;
+        }
+    }
+   
+}
---
+++ UnitTestProject1/EssentialOperationsTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CalculatorNS;
+
+namespace CalculatorNS.Tests
+{
+    [TestClass()]
+    public class EssentialOperationsTest
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Sum(1, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod()]
+        public void minusTest()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Minus(1, 2);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod()]
+        public void methodWihoutFullCoverageTest()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.methodWithoutFullCoverage(10);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
+
+
