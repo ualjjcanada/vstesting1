d2c1ff73ef542367580fa88d9e9e03050352f64e
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2018-05-24 13:46:22 +0200

    new failed tests

--- UnitTestProject1/EssentialOperationsTest.cs
+++ UnitTestProject1/EssentialOperationsTest.cs
@@ -17,11 +17,37 @@ namespace CalculatorNS.Tests
         }
 
         [TestMethod()]
+        public void SumTest2()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Sum(2, 2);
+            Assert.AreEqual(5, result);
+        }
+
+
+        [TestMethod()]
+        public void SumTest3()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Sum(1, 6);
+            Assert.AreEqual(8, result);
+        }
+
+
+        [TestMethod()]
         public void minusTest()
         {
             var Calculator = new Calculator();
             var result = Calculator.Minus(1, 2);
             Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod()]
+        public void minusTest2()
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Minus(4, 2);
+            Assert.AreEqual(-2, result);
         }
 
         [TestMethod()]
